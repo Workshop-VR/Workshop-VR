bef5e222c13a8589e77392a3d649fbe7e09710c9
GpasDeBlase <pierre.erme@gmail.com>
2024-03-11 11:19:32 +0100

    Add Flamme révélatrice
    Ajout de la scène de code
    Ajout de la bougie
    Ajout du papier avec indice
    Ajout de matériaux
    Ajout du script qui affiche le texte quand le papier entre dans une zone de lumière "chaude"

---
+++ Assets/Scripts/TextAppears.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TextAppears : MonoBehaviour
+{
+	private TextMeshPro indice;                         
+
+	void Start()
+	{
+        indice = GameObject.Find("IndiceText").GetComponent<TextMeshPro>();
+		indice.color = new Color(0, 0, 0, 0);                                       // Set la couleur du texte et de l'alpha a 0 (non visible de base)
+	}
+
+
+	void Update()
+	{
+
+	}
+
+    private void OnTriggerEnter(Collider light)
+    {
+		if (light.gameObject.layer == 3)                                            // Check si l'object est rentré dans une zone de chaleur pour faire apparaitre le texte
+		{
+			indice.color = new Color(0, 0, 0, 1);                                   // Change l'alpha a 1
+		}
+        
+    }
+
+    private void OnTriggerExit(Collider light)
+    {
+        if (light.gameObject.layer == 3)
+        {
+            indice.color = new Color(0, 0, 0, 0);                                   // Remet l'alpha a 0 quand l'objet quitte le collider 
+        }
+
+
+    }
+}
