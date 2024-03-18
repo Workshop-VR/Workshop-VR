1bbf7a8f8323657306b7b3df1f546479b515d15e
Lyshaka <maxence.daboville@outlook.fr>
2024-03-18 11:13:18 +0100

    Add full dynamic light

--- Assets/Scripts/LookAtCamera.cs
+++ Assets/Scripts/LookAtCamera.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class LookAtCamera : MonoBehaviour
 {
+	private Transform pivot;
+	private Camera cam;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+		pivot = GetComponent<Transform>();
+        cam = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        pivot.LookAt(cam.transform);
     }
 }
