120ce3a3a9dbf0cadea621ae33a988edf3ae5285
Lyshaka <maxence.daboville@outlook.fr>
2024-03-21 15:21:04 +0100

    Update Basketball Sounds
    Ajout des sons pour le panier de basket

--- Assets/Scripts/BasketGoal.cs
+++ Assets/Scripts/BasketGoal.cs
@@ -8,9 +8,11 @@ public class BasketGoal : MonoBehaviour
 	[SerializeField] private BasketDetector bd1;
 	[SerializeField] private BasketDetector bd2;
 	[SerializeField] private TextMeshPro pointText;
+	[SerializeField] private AudioClip audioPoint;
+	[SerializeField] private AudioClip audioSuperPoint;
 
+	private AudioSource audioSource;
 	private int points = 0;
-
 	private bool hasEntered1 = false;
 	
 	public void Check()
@@ -20,6 +22,8 @@ public class BasketGoal : MonoBehaviour
 			//Debug.Log("Point !");
 			hasEntered1 = false;
 			points++;
+			audioSource.clip = points % 10 == 0 ? audioSuperPoint : audioPoint;
+			audioSource.Play();
 			pointText.text = "" + points;
 		}
 		if (bd1.GetHasEntered() && !bd2.GetHasEntered())
@@ -31,7 +35,7 @@ public class BasketGoal : MonoBehaviour
 	// Start is called before the first frame update
 	void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
